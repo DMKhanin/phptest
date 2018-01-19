0f88c9548abc391e945d0d09954e66c6466c93c1
Dmitriy Khanin <31586524+DMKhanin@users.noreply.github.com>
2018-01-19 09:40:42 +0300

    add comments
    add comments to save.php

--- save.php
+++ save.php
@@ -17,49 +17,49 @@
             <div class="block" style="text-align: center">
                 <?php
 
-                require_once ("config/database.php");
-                if (!empty($_POST["username"])){
-                    $mysqli = new mysqli($dbip,$dblogin,$dbpassword,$dbname);
-                    $sql = "SELECT * FROM vopros LIMIT 10";
-                    $result = $mysqli -> query($sql);
-                    $otvetsarray = array();
-                    $i = 0;
+                require_once ("config/database.php"); //подключение файла базы данных
+                if (!empty($_POST["username"])){ //проверка на пустое поле фамилии имени отчества
+                    $mysqli = new mysqli($dbip,$dblogin,$dbpassword,$dbname); //создаём объект бд и подключаемся
+                    $sql = "SELECT * FROM vopros LIMIT 10"; //формируем запрос к базе данных
+                    $result = $mysqli -> query($sql); //отправляем запрос в базу данных
+                    $otvetsarray = array(); //создаём переменную типа массив
+                    $i = 0; //создаём итератор
 
-                    $vernih = 0;
-                    $nevernih = 0;
+                    $vernih = 0; //создаём переменную для верных ответов
+                    $nevernih = 0; //создаём переменную для не верных ответов
 
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $otvetsarray[$i] = $row["otvet"];
-                        $i++;
+                    while ($row = mysqli_fetch_assoc($result)) { //преобразуем результат полученный из mysql в ассоциативный массив пока результат имеет мевсто быть
+                        $otvetsarray[$i] = $row["otvet"]; //присваиваем в массив ответ
+                        $i++; //увеличиваем итератор на 1
                     }
 
                     //4 I
 
-                    for ($q = 0; $q <= $i-1;$q++){
+                    for ($q = 0; $q <= $i-1;$q++){ //запускаем цикл для проверки от 0 до количества вопросов
                         //echo "---- <br>";
                         //  print_r($_POST[$q]);
-                        if(count($_POST[$q])>1){
-                            $_POST[$q] = implode(",", $_POST[$q]);
-                        } else {
-                            $_POST[$q] = $_POST[$q][0];
+                        if(count($_POST[$q])>1){ // если мы получили из формы массив содержпщий более одного элемента тогда
+                            $_POST[$q] = implode(",", $_POST[$q]); //склеиваем его в строку через запятую
+                        } else { //иначе
+                            $_POST[$q] = $_POST[$q][0]; //просто вытаскивем нулевой элемент из него
                         }
                         //echo $_POST[$q] . " = " . $otvetsarray[$q] . "<br>";
-                        if ($_POST[$q] == $otvetsarray[$q]){
-                            $vernih++;
-                        } else {
-                            $nevernih++;
+                        if ($_POST[$q] == $otvetsarray[$q]){ //если ответ полученный из формы равен ответу в массиве тогда
+                            $vernih++; //увеличиваем счётчик верных ответов на 1
+                        } else { //иначе
+                            $nevernih++; //увеличиваем счётчик не верных ответов на 1
                         }
-                        $arr[$q] = $result;
+                        $arr[$q] = $result; 
                     }
 
 
 
-                    $sql = "INSERT INTO users (username,vernih,nevernih) VALUES ('" . $_POST["username"] . "','" . $vernih . "','" .  $nevernih . "')";
-                    $mysqli -> query($sql);
-                    echo "<p class='logotype' style='padding: 15px'>" . $_POST["username"] . "</p>";
-                    echo "Верных ответов: " . $vernih . "<br>Не верных ответов: " . $nevernih;
-                } else {
-                    echo "Пустое имя пользователя!";
+                    $sql = "INSERT INTO users (username,vernih,nevernih) VALUES ('" . $_POST["username"] . "','" . $vernih . "','" .  $nevernih . "')"; //формируем запрос к базе данных на запись пользователя
+                    $mysqli -> query($sql); //отправляем запрос
+                    echo "<p class='logotype' style='padding: 15px'>" . $_POST["username"] . "</p>"; //выводим имя пользователя на страницу
+                    echo "Верных ответов: " . $vernih . "<br>Не верных ответов: " . $nevernih; //выводим результат выполнения теста на страницу
+                } else { //иначе
+                    echo "Пустое имя пользователя!"; //выводим сообщение о пустом имени пользователя
                 }
                 ?>
             </div>
